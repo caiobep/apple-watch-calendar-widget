141b67ce42eb7e8b415d4c24b68629b8f3140f38
Caio Amaral Corrêa <caiobep@me.com>
2020-09-06 18:55:38 -0300

    chore: check code quality using eslint

--- src/apple-watch-calendar.test.tsx
+++ src/apple-watch-calendar.test.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { render } "@testing-library/react"
+import React from 'react'
 import { render as Component } from './apple-watch-calendar'
+import { render } from '@testing-library/react'
 
-describe("Apple Watch Calendar", () => {
-  it("Displays event names", () => {
-        const mockCommandResult = `Work on Apple Watch Calendar Widget For macOS,16:00 - 17:00`;
+describe('Apple Watch Calendar', () => {
+  it('Displays event names', () => {
+    const mockCommandResult = 'Work on Apple Watch Calendar Widget For macOS,16:00 - 17:00'
 
-    const { getByText } = render(<Component output={mockCommandResult} />);
+    const { getByText } = render(<Component output={mockCommandResult} />)
 
     expect(getByText(/work on apple watch calendar widget for macOS/gi)).toBeVisible()
     expect(getByText(/16:00 - 17:00/gi)).toBeVisible()
-  });
-});
+  })
+})
--- src/apple-watch-calendar.tsx
+++ src/apple-watch-calendar.tsx
@@ -1,73 +1,72 @@
-import React from "react";
+import React from 'react'
 
-export const command: string = `/usr/local/bin/icalbuddy -ea -nc -n -iep 'datetime, title' -b '' -ps '|,|' eventsToday`;
+export const command = '/usr/local/bin/icalbuddy -ea -nc -n -iep \'datetime, title\' -b \'\' -ps \'|,|\' eventsToday'
 
-const minutes = (min: number) => min * 60 * 1000;
+const minutes = (min: number) => min * 60 * 1000
 
-export const refreshFrequency = minutes(1);
+export const refreshFrequency = minutes(1)
 
 interface Event {
-    id: string;
-    name: string;
-    startTime: string;
-    endTime: string;
-    duration: number;
+  id: string
+  name: string
+  startTime: string
+  endTime: string
+  duration: number
 }
 
 const convertiCalOutputToJson = (iCalOutput: string): Event[] => {
-    return iCalOutput
-        .trim()
-        .split("\n")
-        .slice(0, 3)
-        .map((eventText) => {
-            const [name, time] = eventText.split(",");
-            const [startTime, endTime] = time.split("-");
+  return iCalOutput
+    .trim()
+    .split('\n')
+    .slice(0, 3)
+    .map((eventText) => {
+      const [name, time] = eventText.split(',')
+      const [startTime, endTime] = time.split('-')
 
-            const duration =
-                Number(endTime.replace(":", "")) - Number(startTime.replace(":", "")) ||
-                0;
+      const duration =
+        Number(endTime.replace(':', '')) - Number(startTime.replace(':', '')) ||
+        0
 
-            return {
-                id: `${eventText}`,
-                name,
-                startTime,
-                endTime,
-                duration,
-            };
-        });
-};
+      return {
+        id: `${eventText}`,
+        name,
+        startTime,
+        endTime,
+        duration
+      }
+    })
+}
 
 const isEventSmallerThanHours = (hours: number) => (event: Event) =>
-    !(event.duration > 0 && event.duration < hours);
+  !(event.duration > 0 && event.duration < hours)
 
-export const render = ({output}: { output: string }): JSX.Element => {
-    const events = convertiCalOutputToJson(output).filter(
-        isEventSmallerThanHours(4)
-    );
+export const render = ({ output }: { output: string }): JSX.Element => {
+  const events = convertiCalOutputToJson(output).filter(
+    isEventSmallerThanHours(4)
+  )
 
-    if (events.length < 1) {
-        return <div/>;
-    }
+  if (events.length < 1) {
+    return <div />
+  }
 
-    const firstEvent = events[0];
+  const firstEvent = events[0]
 
-    return (
-        <div className="container">
-            <div id="vertical-event-calendar-color"/>
-            <div className="text-container">
-                <div id="event-duration" data-testid="event-duration">
-                    {firstEvent.startTime}-{firstEvent.endTime}
-                </div>
-                {events.map((event) => (
-                    <div key={event.id} id="event-name"
-                         data-testid="event-name">
-                        {event.name}
-                    </div>
-                ))}
-            </div>
+  return (
+    <div className="container">
+      <div id="vertical-event-calendar-color" />
+      <div className="text-container">
+        <div id="event-duration" data-testid="event-duration">
+          {firstEvent.startTime}-{firstEvent.endTime}
         </div>
-    );
-};
+        {events.map((event) => (
+          <div key={event.id} id="event-name" data-testid="event-name">
+            {event.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export const className = `
   left: 10px;
@@ -107,4 +106,4 @@ export const className = `
       50% { opacity: 0.5; }
       to { opacity: 1.0; }
   }     
-`;
+`
